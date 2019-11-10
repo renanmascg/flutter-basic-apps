8c27a21341246f4db4f3bac15c49582152690551
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-11-10 18:59:17 -0300

    Cria métodos de verificação se está finalizado o array e que reseta o quiz

--- quizzler_app_flutter/lib/quizBrain.dart
+++ quizzler_app_flutter/lib/quizBrain.dart
@@ -45,4 +45,17 @@ class QuizBrain {
   bool getQuestionAnswer() {
     return _questionBank[_questionNumber].questionAnswer;
   }
+
+  bool isFinished() {
+    if(_questionNumber == _questionBank.length -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void reset() {
+    _questionNumber = 0;
+  }
+  
 }
