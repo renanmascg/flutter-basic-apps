bd8a41e5c250feefdee8dc21d83e4a53878711d5
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-11-12 11:09:26 -0300

    Adiciona funcionalidade para ter somente 1 sexo ativo dentro da aplicação

--- bmi_calculator_flutter_app/lib/input_page.dart
+++ bmi_calculator_flutter_app/lib/input_page.dart
@@ -5,6 +5,8 @@ import 'reusable_card.dart';
 
 const containerColour = Color(0xFF1D1E33);
 const bottomContainerColour = Color(0xFFEB1555);
+const activeCardColour = Color(0xFF1D1E33);
+const inactiveCardColour = Color(0xFF111328);
 
 class InputPage extends StatefulWidget {
   @override
@@ -12,6 +14,32 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+  Color maleCardColour = inactiveCardColour;
+  Color femaleCardColour = inactiveCardColour;
+
+  // 1 - male, 2 - female
+  void updateColour(int gender) {
+    // male card pressed
+    if(gender == 1) {
+      if(maleCardColour == inactiveCardColour) {
+        maleCardColour = activeCardColour;
+        femaleCardColour = inactiveCardColour;
+      } else {
+        maleCardColour = inactiveCardColour;
+      }
+    } else {
+      if (gender == 2) {
+        if(femaleCardColour == inactiveCardColour) {
+          femaleCardColour = activeCardColour;
+          maleCardColour = inactiveCardColour;
+        } else {
+          femaleCardColour = inactiveCardColour;
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,20 +52,34 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(
-                    colour: containerColour,
-                    cardChild: IconContent(
-                      icon: FontAwesomeIcons.mars,
-                      label: 'MALE',
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColour(1);
+                      });
+                    },
+                    child: ReusableCard(
+                      colour: maleCardColour,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
                     ),
                   ),
                 ),
                 Expanded(
-                  child: ReusableCard(
-                    colour: containerColour,
-                    cardChild: IconContent(
-                      icon: FontAwesomeIcons.venus,
-                      label: 'FEMALE',
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColour(2);
+                      });
+                    },
+                    child: ReusableCard(
+                      colour: femaleCardColour,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      ),
                     ),
                   ),
                 )
