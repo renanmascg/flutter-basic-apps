48801ec0af35beee69223714d684e2c82ce33c64
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-11-12 13:26:13 -0300

    Refatora as constantes espalhadas no codigo para um arquivo de constantes

---
+++ bmi_calculator_flutter_app/lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kContainerColour = Color(0xFF1D1E33);
+const kBottomContainerColour = Color(0xFFEB1555);
+const kActiveCardColour = Color(0xFF1D1E33);
+const kInactiveCardColour = Color(0xFF111328);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kLabelTextHeight = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900
+);
