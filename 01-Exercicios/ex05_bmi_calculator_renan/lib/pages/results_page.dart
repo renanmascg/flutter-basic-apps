baaeb0633a9cb7193517a9f8111126a9e8c06dd0
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-11-13 17:22:40 -0300

    Adiciona imagens que serão utilizadas na resposta ao usuário

--- 01-Exercicios/ex05_bmi_calculator_renan/lib/pages/results_page.dart
+++ 01-Exercicios/ex05_bmi_calculator_renan/lib/pages/results_page.dart
@@ -1,11 +1,24 @@
+import 'package:ex05_bmi_calculator_renan/constants.dart';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Response'),
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                kDegradeInitialColor,
+                kDegradeEndColor
+              ]
+            )
+          ),
+
+        ),
       ),
     );
   }
