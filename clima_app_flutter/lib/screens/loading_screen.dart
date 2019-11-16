07730bdb93a74d058dabac0dbbed1f0a45c6d177
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-11-16 18:35:30 -0300

    Adiciona o pacote de http, para fazer requisições a API

--- clima_app_flutter/lib/screens/loading_screen.dart
+++ clima_app_flutter/lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima_app_flutter/services/location.dart';
-
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -14,6 +14,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
     super.initState();
 
     getLocation();
+    getData();
   }
 
   void getLocation() async {
@@ -25,6 +26,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.latitude);
     print(location.longitude);
 
+  }
+
+  void getData() async {
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+
+    if(response.statusCode == 200) {
+      String data = response.body;
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
