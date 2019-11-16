7c9c583a761fb9611c83406df689af96c24f8540
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-11-16 19:38:42 -0300

    Cria o network helper, cuja responsabilidade é realizar a consulta a url da api e retornar uma resposta em formato json, melhorando a organização do código

--- clima_app_flutter/lib/screens/loading_screen.dart
+++ clima_app_flutter/lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
+import 'package:clima_app_flutter/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:clima_app_flutter/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+
+const apiKey = '0b4d8477e565b10d7361e9aaf379b382';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,44 +11,31 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
 
-    getLocation();
-    getData();
+    getLocationData();
+
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     
     Location location = Location();
 
     await location.getCurrentLocation();
 
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
 
-  }
+    NetworkHelper networkHelper = NetworkHelper(url:'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    dynamic weatherData = await networkHelper.getData();
 
-  void getData() async {
-    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    print(weatherData);
 
-    if(response.statusCode == 200) {
-      String data = response.body;
-
-      dynamic decodedData = jsonDecode(data);
-
-      double temperature = decodedData['main']['temp'];
-      int condition = decodedData['weather'][0]['id'];
-      String cityName = decodedData['name'];
-
-      print(temperature);
-      print(condition);
-      print(cityName);
-      
-    } else {
-      print(response.statusCode);
-    }
   }
 
   @override
