6769cd32a6759a733ae8f914a31ecee76f310f69
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-11-17 14:54:41 -0300

    Adiciona transição de pagina e loading enquanto consulta a API externa

--- clima_app_flutter/lib/screens/loading_screen.dart
+++ clima_app_flutter/lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
+import 'package:clima_app_flutter/screens/location_screen.dart';
 import 'package:clima_app_flutter/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:clima_app_flutter/services/location.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = '0b4d8477e565b10d7361e9aaf379b382';
 
@@ -34,12 +36,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
     NetworkHelper networkHelper = NetworkHelper(url:'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
     dynamic weatherData = await networkHelper.getData();
 
-    print(weatherData);
+    Navigator.push(context, 
+    MaterialPageRoute( 
+      builder: (context) =>  LocationScreen()
+    ));
 
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitChasingDots(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
