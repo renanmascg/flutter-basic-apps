d0cd411f90be25f30888dac3c461991c6cf08f0c
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-11-17 20:42:07 -0300

    Refatora o codigo onde weather é responsavel pela consulta a API

--- clima_app_flutter/lib/screens/loading_screen.dart
+++ clima_app_flutter/lib/screens/loading_screen.dart
@@ -1,10 +1,7 @@
 import 'package:clima_app_flutter/screens/location_screen.dart';
-import 'package:clima_app_flutter/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:clima_app_flutter/services/location.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-const apiKey = '0b4d8477e565b10d7361e9aaf379b382';
+import 'package:clima_app_flutter/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -23,15 +20,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getLocationData() async {
     
-    Location location = Location();
-
-    await location.getCurrentLocation();
-
-    NetworkHelper networkHelper = NetworkHelper(
-      url:'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric'
-    );
-
-    dynamic weatherData = await networkHelper.getData();
+    dynamic weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, 
     MaterialPageRoute( 
--- clima_app_flutter/lib/services/weather.dart
+++ clima_app_flutter/lib/services/weather.dart
@@ -1,4 +1,25 @@
+import 'package:clima_app_flutter/services/location.dart';
+import 'package:clima_app_flutter/services/networking.dart';
+
+const apiKey = '0b4d8477e565b10d7361e9aaf379b382';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 class WeatherModel {
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+      url:'$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric'
+    );
+
+    dynamic weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
