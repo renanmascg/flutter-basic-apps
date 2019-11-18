234cc6610a023512f193f67c0e391857a82423a4
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-11-17 21:30:27 -0300

    Cria método que permite consulta pelo nome da cidade

--- clima_app_flutter/lib/services/weather.dart
+++ clima_app_flutter/lib/services/weather.dart
@@ -3,7 +3,21 @@ import 'package:clima_app_flutter/services/networking.dart';
 
 const apiKey = '0b4d8477e565b10d7361e9aaf379b382';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+
+  Future<dynamic> getCityWeather(String cityName) async {
+
+    NetworkHelper networkHelper = NetworkHelper(
+      url: '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric'
+    );
+
+    dynamic weatherData = await networkHelper.getData();
+
+    return weatherData;
+    
+  }
+
 
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
