b09159fff97d3909d33ea49d3178e79bdeb9fc94
Renan Mascarenhas Gonçalves <renanmascg@gmail.com>
2019-11-19 10:23:27 -0300

    Adiciona id para as paginas e rotas, e implementa redirecionamento ao clicar nos botões de login e register

--- flash_chat_app/lib/main.dart
+++ flash_chat_app/lib/main.dart
@@ -15,7 +15,13 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        ChatScreen.id: (context) => ChatScreen()
+      },
+      initialRoute: WelcomeScreen.id,
     );
   }
 }
